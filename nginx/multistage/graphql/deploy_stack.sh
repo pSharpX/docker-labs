d3cbcc730c28b4b03d86e356c4490a5126b733b2
psharpx <crivera2093@gmail.com>
2021-10-20 00:59:35 -0500

    docker multistage builds: compose files

---
+++ nginx/multistage/graphql/deploy_stack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo Validarion docker-compose configuration
+
+# Workaround note: This is only use for docker compose versions under v1.28 becouse --env-file is not supported.
+#eval source <(grep = .env.production)
+#source ./.env.production
+
+# Workaround note: This is only use for docker compose versions under v1.28 becouse --env-file is not supported.
+# Use default name for env file to work: env
+#docker-compose --env-file ./.env.production config 
+docker-compose config 
+
+echo Configuration was successful !!...
+
+read -n 1 -s -r -p "Press any key to continue"
