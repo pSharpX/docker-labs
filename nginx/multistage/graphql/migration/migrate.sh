dbec3f90902b1855f4b306abb8ecc97a1e24524b
psharpx <crivera2093@gmail.com>
2021-10-27 01:15:16 -0500

    fix in dockerfile for database migration

--- nginx/multistage/graphql/migration/migrate.sh
+++ nginx/multistage/graphql/migration/migrate.sh
@@ -3,7 +3,7 @@ CONTAINER_NAME=migration-runner
 
 echo Building $CONTAINER_NAME container.
 
-docker run -rm --name $CONTAINER_NAME --network=graphbook/in.production --env-file .env psharpx/graphbook-migration:1.0
+docker run --rm --name $CONTAINER_NAME --network=graphql_graphbook/in.production --env-file .env psharpx/graphbook-migration:1.0
 
 #--network=graphbook/in.production
 #--add-host=docker:93.184.216.34
